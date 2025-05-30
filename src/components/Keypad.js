dd2966e107172fd57e10a2bbb53ddbe8d26c0401
stephen mburu <stephenmburu8509@gmail.com>
2025-05-30 17:31:21 +0300

    Adds event handling

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,20 @@
 // Code Keypad Component Here
 
-function Keypad (){
+function Keypad() {
+    function handleChange(event) {
+        console.log("Entering password...")
+    }
     return (
-        <div></div>
+        <div>
+            <input
+                type="password"
+                name="Password"
+                placeholder="Enter Password..."
+                onChange={handleChange}
+
+            />
+
+        </div>
     )
 }
 
